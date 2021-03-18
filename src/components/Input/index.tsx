ff13724937c3a221d6c1075ffd4b92c13e3e4aae
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-03-18 11:08:20 -0300

    style: when input is this focus and padding

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
 
@@ -19,6 +19,19 @@ const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
   const { registerField, defaultValue = '', fieldName, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
 
+  const [isFocused, setFocused] = useState(false);
+  const [isFilled, setFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setFocused(false);
+
+    setFilled(!!inputValueRef.current.value);
+  }, []);
+
   useEffect(() => {
     registerField<string>({
       name: fieldName,
@@ -36,14 +49,20 @@ const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
   }, [fieldName, registerField]);
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
+      />
 
       <TextInput
         ref={inputElementRef}
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={value => {
           inputValueRef.current.value = value;
         }}
