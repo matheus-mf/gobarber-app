c25c5060a3d1092530b92c7f60acbfe3c8a1de34
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-03-31 19:04:21 -0300

    feat: creation of app routes

---
+++ src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const CreateAppointment: React.FC = () => {
+  return (
+    <Container>
+      <Text>Appointment Created</Text>
+    </Container>
+  );
+};
+
+export default CreateAppointment;
