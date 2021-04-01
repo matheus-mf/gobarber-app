d716bd85d9b13502dce3a656f3ac0e0112e2ab93
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-04-01 17:55:20 -0300

    feat: formatting date on the appointment created screen

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,6 +1,10 @@
-import React, { useCallback } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import React, { useCallback, useMemo } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+
 import Icon from 'react-native-vector-icons/Feather';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import {
   Container,
@@ -10,28 +14,37 @@ import {
   OkButtonText,
 } from './styles';
 
+interface RouteParams {
+  date: number;
+}
+
 const AppointmentCreated: React.FC = () => {
   const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as RouteParams;
 
   const handleOkPressed = useCallback(() => {
     reset({
-      routes: [
-        {
-          name: 'Dashboard',
-        },
-      ],
+      routes: [{ name: 'Dashboard' }],
       index: 0,
     });
   }, [reset]);
+
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+      { locale: ptBR },
+    );
+  }, [routeParams.date]);
 
   return (
     <Container>
       <Icon name="check" size={80} color="#04d361" />
 
-      <Title>Agendamento Concluído</Title>
-      <Description>
-        Terça, dia 14 de março de 2021 às 12:00h com Natalia Yada
-      </Description>
+      <Title>Agendamento concluído</Title>
+      <Description>{formattedDate}</Description>
 
       <OkButton onPress={handleOkPressed}>
         <OkButtonText>Ok</OkButtonText>
