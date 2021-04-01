f8f5d77cd93e68840b04fae8208cb66c22e010d4
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-04-01 17:29:13 -0300

    style: structure of the created appointment

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+      index: 0,
+    });
+  }, [reset]);
+
   return (
     <Container>
-      <Text>Appointment Created</Text>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento Concluído</Title>
+      <Description>
+        Terça, dia 14 de março de 2021 às 12:00h com Natalia Yada
+      </Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
     </Container>
   );
 };
